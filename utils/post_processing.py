63769a5d1a42a79c590d861bf21ab201bb039b07
i-MaTh <izhangwenjie@vip.qq.com>
2017-07-14 17:03:36 +0800

    Add files via upload

--- utils/post_processing.py
+++ utils/post_processing.py
@@ -5,162 +5,276 @@ import argparse
 from datetime import datetime
 import pickle
 from sklearn.preprocessing import normalize
+from collections import OrderedDict
 
-def merge_component():
-    dis = pickle.load(open('./norm_distances2.pkl', 'r'))
-    trks = np.loadtxt('./tracks07091309_2.txt', delimiter=',').astype(np.int)
-    print type(trks)
-    
-    max_trk_id = trks[:,1].max()
-    min_trk_id = trks[:,1].min()
-    max_frame_id = trks[:,0].max()
-    min_frame_id = trks[:,0].min()
-    print 'max_frame_id: %d, min_frame_id: %d' % (max_frame_id, min_frame_id)
-    print 'max_tracker_id: %d, min_tracker_id: %d' % (max_trk_id, min_trk_id)
-    trk_indices = trks[:,1]
-    print type(trk_indices)
-    threshold = 0.11
-    print 'number of dis < {}: {}'.format(threshold , sum(np.asarray(dis.values()) < threshold))
-    cands = [it for it in dis.items() if it[1] < threshold]
-    #print cands
-    
-    for c in cands:
-        tmp = map(int, c[0].split('_'))
-        trk_indices = [tmp[0] if idx == tmp[1] else idx for idx in trk_indices]
-        
-    trks[:,1] = trk_indices
-    np.savetxt('refine_res2.txt', trks, fmt='%.2f', delimiter=',')
-    
-        
-def compute_similarity(trk_reid_file):
-   
-    trk_reid = np.load(trk_reid_file) # [frame_id, tracker_id, bb_left, bb_top, bb_width, bb_height, -1, -1, -1, reid_features]
-    trks = trk_reid[:,:10].astype(np.int)
-    #print trks.shape
-    reid_features = trk_reid[:,10:]
-    reid_features = normalize(reid_features) # normalize
 
-    #print reid_features.shape
-    max_trk_id = trks[:,1].max()
-    min_trk_id = trks[:,1].min()
-    max_frame_id = trk_reid[:,0].max()
-    min_frame_id = trk_reid[:,0].min()
-    print 'max_frame_id: %d, min_frame_id: %d' % (max_frame_id, min_frame_id)
-    print 'max_tracker_id: %d, min_tracker_id: %d' % (max_trk_id, min_trk_id)
+def reassign_id(trk_reid_file1, trk_reid_file2):
+	trk_reid1 = np.load(trk_reid_file1)
+	#print 'trk_reid shape: {}'.format(trk_reid1.shape)
+	trk_reid2 = np.load(trk_reid_file2)
+	#print 'trk_reid shape: {}'.format(trk_reid2.shape)
+	indices_trk1 = trk_reid1[:,1].astype(np.int)
+	indices_trk2 = trk_reid2[:,1].astype(np.int)
 	
-    trk_indices = trks[:,1]
-    distances = {}
-    k = 3
-    count = 0
-    start_time = datetime.now()
-    for i in range(min_trk_id, max_trk_id):
-        mask_i = (trk_indices == i)
-        reid_i = reid_features[mask_i]
-        if reid_i.shape[0] == 0: continue
-        for j in range(i+1, max_trk_id+1):
-            mask_j = (trk_indices == j)
-            reid_j = reid_features[mask_j]
-            if reid_j.shape[0] ==0: continue
-            #print reid_j.shape
-            dis = cdist(reid_i, reid_j, metric = 'cosine')
-            key = '%d_%d' % (i, j)
-            distances[key] = np.mean(sorted(dis.ravel())[:k])
-            #distances[key] = dis.min()
-            count += 1
-            if count % 100 == 0:
-                print 'process: %d' % count
-            
-	pickle.dump(distances, open('norm_distances2_3.pkl', 'wb'))
+	unique_id_trk1 = set()
+	for idx in indices_trk1:
+		unique_id_trk1.add(idx)
+
+	unique_id_trk2 = set()
+	for idx in indices_trk2:
+		unique_id_trk2.add(idx)
+
+	print 'number of unique id in trk1: %d' % len(unique_id_trk1)
+	print 'number of unique id in trk2: %d' % len(unique_id_trk2)
+
+	id_set1 = {k:(v+1) for v, k in enumerate(unique_id_trk1)}
+	id_set2 = {k:(v+1+len(unique_id_trk1)) for v, k in enumerate(unique_id_trk2)}
+	
+	print id_set1
+	print id_set2
+	
+	indices_trk1 = [id_set1[idx] for idx in indices_trk1]
+	trk_reid1[:, 1] = indices_trk1
+	indices_trk2 = [id_set2[idx] for idx in indices_trk2]
+	trk_reid2[:, 1] = indices_trk2
+
+	np.save(trk_reid_file1, trk_reid1)
+	np.save(trk_reid_file2, trk_reid2)
+
+
+def merge_camera(trk_reid_file1, trk_reid_file2, threshold=0.14):
+	trk_reid1 = np.load(trk_reid_file1)
+	trk_reid2 = np.load(trk_reid_file2)
+	indices_trk1 = trk_reid1[:,1].astype(np.int)
+	indices_trk2 = trk_reid2[:,1].astype(np.int)
+
+	unique_id_trk1 = set()
+	for idx in indices_trk1:
+		unique_id_trk1.add(idx)
+	print unique_id_trk1
+
+	unique_id_trk2 = set()
+	for idx in indices_trk2:
+		unique_id_trk2.add(idx)
+	print unique_id_trk2
+
+	#print unique_id_trk1
+	print 'number of unique id in trk1: %d' % len(unique_id_trk1)
+	print 'number of unique id in trk2: %d' % len(unique_id_trk2)
+	
+
+	reid_features1 = trk_reid1[:, 10:]
+	reid_features2 = trk_reid2[:, 10:]
+	distances = OrderedDict()
+
+	for i in unique_id_trk1:
+		mask_i = (indices_trk1 == i)
+		reid_i = reid_features1[mask_i]
+		for j in unique_id_trk2:
+			mask_j = (indices_trk2 == j)
+			reid_j = reid_features2[mask_j]
+			dis = cdist(reid_i, reid_j, metric = 'cosine')
+
+			key = '1-%d_2-%d' % (i, j)
+			distances[key] = dis.min()
+
+	#pickle.dump(distances, open('two_camera_distance.pkl', 'wb'))
+	#print distances	
+	cands = [it for it in distances.items() if it[1] < threshold]
+	print cands
+	
+	for c in cands:
+		tmp = c[0].split('_')
+		i = map(int, tmp[0].split('-'))[1]
+		j = map(int, tmp[1].split('-'))[1]
+		print 'i: %d, j: %d' % (i, j)
+		indices_trk2 = [i if idx == j else idx for idx in indices_trk2]
+	
+	trk_reid2[:, 1] = indices_trk2
+	
+	np.save(trk_reid_file2, trk_reid2)
+	
+
+def merge_component(trks_file, dis_file, output_file=None, threshold=0.1):
+	dis = pickle.load(open(dis_file, 'r'))
+	#trks = np.loadtxt(trks_file, delimiter=',').astype(np.int)
+	trk_reid = np.load(trks_file)
+	trks = trk_reid[:, :10].astype(np.int)
+	
+	max_trk_id = trks[:,1].max()
+	min_trk_id = trks[:,1].min()
+	max_frame_id = trks[:,0].max()
+	min_frame_id = trks[:,0].min()
+	print 'max_frame_id: %d, min_frame_id: %d' % (max_frame_id, min_frame_id)
+	print 'max_tracker_id: %d, min_tracker_id: %d' % (max_trk_id, min_trk_id)
+	trk_indices = trks[:,1]
+	
+	print 'number of dis < {}: {}'.format(threshold , sum(np.asarray(dis.values()) < threshold))
+	cands = [it for it in dis.items() if it[1] < threshold]
+	#print cands		    
+	for c in cands:
+		tmp = map(int, c[0].split('_'))
+		trk_indices = [tmp[0] if idx == tmp[1] else idx for idx in trk_indices]
+									        
+	#trks[:,1] = trk_indices
+	#np.savetxt(output_file, trks, fmt='%.2f', delimiter=',')
+	trk_reid[:,1] = trk_indices
+	np.save(trks_file, trk_reid)
+
+
+def compute_similarity(trk_reid_file, norm=False):
+	'''
+	trks = np.loadtxt(trk_file, delimiter=',')
+	num_per_trk = {i:len(trks[trks[:,1].astype(np.int) == i]) for i in range(min_trk_id, max_trk_id+1)}
+	#print num_per_trk
+	'''
+	trk_reid = np.load(trk_reid_file) # [frame_id, tracker_id, bb_left, bb_top, bb_width, bb_height, -1, -1, -1, reid_features]
+	trks = trk_reid[:,:10].astype(np.int)
+	#print trks.shape
+	reid_features = trk_reid[:,10:]
+	if norm:
+		reid_features = normalize(reid_features) # normalize
+
+	#print reid_features.shape
+	max_trk_id = trks[:,1].max()
+	min_trk_id = trks[:,1].min()
+	max_frame_id = trk_reid[:,0].max()
+	min_frame_id = trk_reid[:,0].min()
+	print 'max_frame_id: %d, min_frame_id: %d' % (max_frame_id, min_frame_id)
+	print 'max_tracker_id: %d, min_tracker_id: %d' % (max_trk_id, min_trk_id)
+	
+	trk_indices = trks[:,1]
+	distances = OrderedDict()
+
+	start_time = datetime.now()
+
+	for i in range(min_trk_id, max_trk_id):
+		mask_i = (trk_indices == i)
+		reid_i = reid_features[mask_i]
+		if reid_i.shape[0] == 0: continue
+		for j in range(i+1, max_trk_id+1):
+			mask_j = (trk_indices == j)
+			reid_j = reid_features[mask_j]
+			if reid_j.shape[0] == 0: continue
+			dis = cdist(reid_i, reid_j, metric = 'cosine')
+			key = '%d_%d' % (i, j)
+			distances[key] = dis.min()
+
+		'''
+		values = []
+		for j in range(min_trk_id, max_trk_id+1):
+			mask_j = (trk_indices == j)
+			reid_j = reid_features[mask_j]
+			if reid_j.shape[0] ==0: continue
+			
+			if i != j:
+				dis = cdist(reid_i, reid_j, metric = 'cosine')
+				#v = dis.max()
+				v = dis.min()
+			else:
+				v = 0
+			values.append(v)
+		distances[i] = values
+		'''
+	
+	outpath = os.path.splitext(trk_reid_file)[0] + '_distance_lookup.pkl'
+	pickle.dump(distances, open(outpath, 'wb'))
 
 	end_time = datetime.now()
 	duration = end_time - start_time
 	print 'duration time: %s' % duration
 
-	'''		
+
+def clean_tracker(trk_reid_file, threshold=10):
+	trk_reid = np.load(trk_reid_file)
+	trks = trk_reid[:,:10].astype(np.int)
+	min_trk_id = trks[:,1].min()
+	max_trk_id = trks[:,1].max()
+	s = set()
+	for i in trks[:,1]:
+		s.add(i)
+	print s
+	num_per_trk = {i:len(trks[trks[:,1] == i]) for i in range(min_trk_id, max_trk_id+1)}
+	print num_per_trk
+	print sorted(num_per_trk.values())
+	
+		
 	new_trks = []
-	# filter
+	for i in range(trks.shape[0]):
+		trk_id = trks[i][1]
+		bb_left = trks[i][2]
+		bb_top = trks[i][3]
+		if num_per_trk[trk_id] < threshold: continue
+		if bb_left > 1700 or bb_top > 800: continue
+		new_trks.append(trk_reid[i])
+
+	#_tuples = os.path.splitext(trk_reid_file)
+	#outpath = _tuples[0] + '_new' + _tuples[1]
+	outpath = trk_reid_file
+	np.save(outpath, new_trks)
 	
-	for i in range(len(trks)):
-		#print num_per_trk[int(trks[i][1])]
-		key = int(trks[i][1])
-		if num_per_trk[key] < 5:
-			continue
-		new_trks.append(trks[i])
-	
-	np.savetxt('new_trackers0707_2.txt', new_trks,delimiter=',', fmt='%.2f')
-	'''
 
 def parse_args():
 	""" 
 	Parse command line arguments.
 	"""
 	parser = argparse.ArgumentParser(description="Post-Processing Tracker Result")
-	#parser.add_argument("--tracker_file", help="Path to tracker file.", default=None, required=True)
-	parser.add_argument("--trk_reid_file", help="Path to reid of tracker file.", default=None)
-	parser.add_argument("--output_file", help="Path to output file.", default=None)
+	parser.add_argument("-dis_file", help="Path to tracker file.", default=None)
+	parser.add_argument("-trk_reid_file", help="Path to reid of tracker file.", default=None)
+	parser.add_argument("-output_file", help="Path to output file.", default=None)
 	return parser.parse_args()
 
-def test(path):
+def test(path, threshold=0.3):
 	#path = './demo_test_data/distances.pkl'
 	
 	dis = pickle.load(open(path, 'r'))
-	max_dis = max(dis.values())
-	min_dis = min(dis.values())
-	mean_dis = np.mean(dis.values())
-	median_dis = np.median(dis.values())
+	# dis.items() type is list	
+	'''
+	trk_indices = {i:dis.keys()[i] for i in range(len(dis))}	
+	matches = {}
+	for k in dis:
+		#min_dis = sorted(dis[k])
+		min_idx = np.argsort(dis[k])[1]
+		kt = trk_indices[min_idx]
+		key = '%d_%d' % (k, kt)
+		value = dis[k][min_idx]
+		matches[key] = value
+	'''
+	matches = dis
+
+	max_dis = max(matches.values())
+	min_dis = min(matches.values())
+	mean_dis = np.mean(matches.values())
+	median_dis = np.median(matches.values())
 	print 'max_dis: {}, min_dis: {}'.format(max_dis, min_dis)
 	print 'mean_dis: {}, median_dis: {}'.format(mean_dis, median_dis)
-	threshold = 0.11
-	print 'number of dis < {}: {}'.format(threshold , sum(np.asarray(dis.values()) < threshold))
-	print dis['43_89']
-	candidates = [it for it in dis.items() if it[1] < threshold]
+	print 'number of dis < {}: {}'.format(threshold , sum(np.asarray(matches.values()) < threshold))
+	print dis	
+	candidates = [it for it in matches.items() if it[1] < threshold]
 	
 	print candidates
 	#trks_path = './demo_test_data/tracks0707_2.txt'
 	#trks = np.loadtxt(trks_path, delimiter=',')
+
+
+def convert_format(inpath, outpath):
+	trk_reid = np.load(path)
+	trks = trk_reid[:,:10].astype(np.int)
+	#print trks.shape
+	reid_features = trk_reid[:,10:]
 	
 
-def convert_format(inpath):
-    #import OrderDict
-    trk_reid = np.load(inpath)
-    trks = trk_reid[:,:10].astype(np.int)
-    #print trks.shape
-    #reid_features = trk_reid[:,10:]
-    #print reid_features.shape
-    
-    max_trk_id = trks[:,1].max()
-    min_trk_id = trks[:,1].min()
-    max_frame_id = trk_reid[:,0].max()
-    min_frame_id = trk_reid[:,0].min()
-    print 'max_frame_id: %d, min_frame_id: %d' % (max_frame_id, min_frame_id)
-    print 'max_tracker_id: %d, min_tracker_id: %d' % (max_trk_id, min_trk_id)
-	
-    trk_indices = trks[:,1]
-    res = {}
-    for i in range(min_trk_id, max_trk_id+1):
-        mask = (trk_indices == i)
-        if sum(mask) == 0: continue
-        #print trk_reid[mask].shape
-        values = []
-        for row in trk_reid[mask]:
-            tmp = []
-            tmp.append(int(row[0]))
-            tmp.append(row[2:6].tolist())
-            tmp.append(row[6:].tolist())
-            values.append(tmp)
-
-        res[i] = values
-
-    pickle.dump(res, open('result1.pkl', 'wb'))
 
 
 if __name__ == '__main__':
-    #args = parse_args()
-    compute_similarity('./trk_reid_07091309_2.npy')
-    #test('norm_distances1.pkl')	
-    #path = './trk_reid_07091309.npy'
-    #convert_format(path)
-    #merge_component()
+	#args = parse_args()
+	#test(sys.argv[1])	
+	#clean_tracker(args.trk_reid_file)
+	#compute_similarity(args.trk_reid_file, True)
+	#test(args.dis_file)
+	#merge_component(args.trk_reid_file, args.dis_file)
+	#reassign_id(sys.argv[1], sys.argv[2])
+	merge_camera(sys.argv[1], sys.argv[2])
 
 
 
--- utils/vis_detections.py
+++ utils/vis_detections.py
@@ -1,17 +1,13 @@
 import numpy as np
-<<<<<<< HEAD
 #import h5py
-=======
->>>>>>> 826c5be7aab7f79158cc1c26a1bfd42b9cd1bf60
 import os
 import sys
 import matplotlib.pyplot as plt 
 import matplotlib.patches as patches
 from random import random as rand
-
+import cv2
 
 def show_boxes():
-<<<<<<< HEAD
 	img_dir = sys.argv[1]
 	dets_path = sys.argv[2]
 	idx = int(sys.argv[3])
@@ -45,51 +41,12 @@ def show_boxes():
 	plt.gca().text(bbox[0], bbox[1], 'ID: {}'.format(idx), 
 				bbox=dict(facecolor=color, alpha=0.5), fontsize= 7, color='white')
 	plt.show()
-=======
-    img_dir = './img1'
-    dets_path = './tracks07091309.txt'
-    #dets_path = './refine_res1.txt'
-    idx = int(sys.argv[1])
-    scale = 1.0
-
-    dets_in = np.loadtxt(dets_path, delimiter=',').astype(np.int)
-    det = dets_in[dets_in[:,1] == idx][0]
-    img_path = os.path.join(img_dir, str(det[0]).zfill(6) + '.jpg')
-    print img_path
-
-    plt.cla()
-    plt.axis('off')
-    img = plt.imread(img_path)
-    print np.shape(img)
-    plt.imshow(img)
-	
-
-    bbox = det[2:6] * scale
-    color = (rand(), rand(), rand())
-    # rectangle params: lower left(x,y), weight, height
-    rect = plt.Rectangle((bbox[0], bbox[1]), 
-						  bbox[2], bbox[3],
-						  fill = False, edgecolor = color, 
-						  linewidth = 2.5)
-    plt.gca().add_patch(rect)
-		
-
-    idx = det[1]
-    plt.gca().text(bbox[0], bbox[1], 'ID: {}'.format(idx), 
-				bbox=dict(facecolor=color, alpha=0.5), fontsize= 7, color='white')
-    plt.show()
->>>>>>> 826c5be7aab7f79158cc1c26a1bfd42b9cd1bf60
 
 
 def show_tracks(img_dir, det_path, out_dir, scale=1.0):
-	import cv2
-<<<<<<< HEAD
 	
-	#dets_in = np.load(det_path)
-=======
-
->>>>>>> 826c5be7aab7f79158cc1c26a1bfd42b9cd1bf60
-	dets_in = np.loadtxt(det_path, delimiter=',')
+	dets_in = np.load(det_path)
+	#dets_in = np.loadtxt(det_path, delimiter=',')
 	img_paths = {int(os.path.splitext(f)[0]): os.path.join(img_dir, f) for f in os.listdir(img_dir)}
 	img_names = {int(os.path.splitext(f)[0]): f for f in os.listdir(img_dir)}
 	frame_indices = dets_in[:, 0].astype(np.int)
@@ -97,7 +54,13 @@ def show_tracks(img_dir, det_path, out_dir, scale=1.0):
 	max_frame_idx = max(frame_indices)
 	min_frame_idx = min(frame_indices)
 	
-	colours = [(int(100*rand()), int(100*rand()), int(100*rand())) for _ in range(int(max(dets_in[:,1])))]
+	#colours = (100 * np.random.rand(25, 3)).astype(np.int)
+	colours = [(100,149,237), (78,238,148), (127,255,0), (238,238,0), (255,106,106), 
+				(255,127,36), (255,20,147), (255,52,179), (155,48,255), (144,238,144),
+				(0,191,255), (0,0,205), (105,89,205), (255,228,225), (255,222,173),
+				(139,137,137), (160,32,240), (255,0,255), (199,21,133), (255,69,0),
+				(250,128,114),(50,205,50), (0,0,255)
+				]
 	print 'colours size: %d' % len(colours)
 	print 'max_id: %d, min_id: %d' % (max(dets_in[:, 1].astype(np.int)), min(dets_in[:, 1].astype(np.int)))
 	for i in range(min_frame_idx, max_frame_idx):
@@ -109,18 +72,20 @@ def show_tracks(img_dir, det_path, out_dir, scale=1.0):
 		for d in sub_dets:
 			d = d.astype(np.int)
 			cv2.rectangle(img, (d[1], d[2]),(d[1] + d[3],d[2] + d[4]),
-							  color = colours[d[0]-1], thickness = 3)
+							  color = colours[d[0]], thickness = 3)
 			'''
 			cv2.rectangle(img, (d[1] - 7, d[2] - 7), 
 				    (d[1] + 5, d[2] + 5),
 				    color = colours[d[0]], thickness = -1)
 			'''
-<<<<<<< HEAD
-			cv2.putText(img, 'ID:{}'.format(d[0]), (d[1], d[2]),
-=======
-			cv2.putText(img, 'Person ID:{}'.format(d[0]), (d[1], d[2]),
->>>>>>> 826c5be7aab7f79158cc1c26a1bfd42b9cd1bf60
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, color=(255, 255, 255),
+			if d[0] in [5, 6, 7]:
+			#if d[0] in [2, 3]:
+				cv2.putText(img, 'From C1', (d[1]+d[3]+5, d[2]+17), cv2.FONT_HERSHEY_SIMPLEX, 1.1,
+						color = (255,255,255),
+						thickness = 3
+						)
+			cv2.putText(img, 'ID:{}'.format(d[0]), (d[1]+5, d[2]-8),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.9, color=(255, 255, 255),
 					thickness = 2)
 			
 		out_path = os.path.join(out_dir, img_names[i])
@@ -129,11 +94,8 @@ def show_tracks(img_dir, det_path, out_dir, scale=1.0):
 		if i % 100 == 0:
 			print 'process: %d' % i
 
-<<<<<<< HEAD
+
 def show_video(video_dir, dets_path, scale=1.0):
-=======
-def show_boxes3(video_dir, dets_path, scale=1.0):
->>>>>>> 826c5be7aab7f79158cc1c26a1bfd42b9cd1bf60
 	"""
 	det format: frame_id, -1, bbox_left, bbox_right, width, height, score
 	"""
@@ -141,7 +103,8 @@ def show_boxes3(video_dir, dets_path, scale=1.0):
 	import time
 	import os
 
-	dets_in = np.loadtxt(dets_path, delimiter=',') #load detections, format: ndarray
+	#dets_in = np.loadtxt(dets_path, delimiter=',') #load detections, format: ndarray
+	dets_in = np.load(dets_path)
 	#img_dir = os.path.join(video_dir, 'img2')
 	img_dir = video_dir
 	img_paths = {int(os.path.splitext(f)[0]): os.path.join(img_dir, f) for f in os.listdir(img_dir)}
@@ -183,37 +146,100 @@ def show_boxes3(video_dir, dets_path, scale=1.0):
 		plt.draw()
 		ax1.cla()
 
+def post_processing(img1_dir, img2_dir, output_dir):
+	img1_paths = {int(os.path.splitext(f)[0]): os.path.join(img1_dir, f) for f in os.listdir(img1_dir)}
+	img2_paths = {int(os.path.splitext(f)[0]): os.path.join(img2_dir, f) for f in os.listdir(img2_dir)}
+	img_names = {int(os.path.splitext(f)[0]): f for f in os.listdir(img1_dir)}
+
+	for idx, key in enumerate(img1_paths):
+		img1 = cv2.imread(img1_paths[key])
+		img2 = cv2.imread(img2_paths[key])
+				
+		img1 = img1[:1000, :, :]
+		img1 =cv2.resize(img1, (1920, 1320))
+			
+		img2 = img2[:1000, :, :]
+		img2 =cv2.resize(img2, (1920, 1320))
+		img = np.concatenate((img1, img2), axis=1)
+		
+		alpha = 0.5
+		position_x = 500
+		overlay = img.copy()
+		output = img.copy()
+		cv2.rectangle(overlay, (position_x + 20, 20),(position_x + 620, 130),
+			              color = (250, 235,215), thickness = -1)
+
+		cv2.putText(overlay, 'Camera ID: C1', (position_x + 30, 100),
+			            cv2.FONT_HERSHEY_SIMPLEX, 2.5, color=(255,0,0),
+						            thickness = 5)
+
+		cv2.rectangle(overlay, (1920 + position_x + 20, 20),(1920 + position_x + 620, 130),
+			              color = (250, 235,215), thickness = -1)
+
+		cv2.putText(overlay, 'Camera ID: C2', (1920 + position_x + 30, 100),
+			            cv2.FONT_HERSHEY_SIMPLEX, 2.5, color=(255,0,0),
+						            thickness = 5)
+		# apply the overlay
+		cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+
+		output_path = os.path.join(output_dir, img_names[key])
+		#print output_path
+		cv2.imwrite(output_path, output)
+		
+		if idx % 100 == 0: print 'process: %d' % idx
+		
+		#break
+
+
+def refine(img_dir, output_dir):
+	img_paths = {int(os.path.splitext(f)[0]): os.path.join(img_dir, f) for f in os.listdir(img_dir)}
+	img_names = {int(os.path.splitext(f)[0]): f for f in os.listdir(img_dir)}
+	
+	alpha = 0.7
+	position_x = 1330
+
+	for idx, key in enumerate(img_paths):
+		img = cv2.imread(img_paths[key])
+		#print img.shape
+		overlay = img.copy()
+		output = img.copy()
+		cv2.rectangle(overlay, (position_x + 20, 120), (position_x + 960, 230),
+			              color = (250, 235,215), thickness = -1)
+
+		cv2.putText(overlay, 'Cross Camera ReID', (position_x + 30, 200),
+			            cv2.FONT_HERSHEY_SIMPLEX, 3, color=(0,0,255),
+						            thickness = 8)
+
+		cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+		
+		#output = cv2.resize(output, (3840, 1220))
+		#print output.shape
+		output_path = os.path.join(output_dir, img_names[key])
+		cv2.imwrite(output_path, output)
+
+		if idx % 100 == 0: 
+			print 'process: %d' % idx
+
 
 if __name__ == '__main__':
 	#img_path = sys.argv[1]	
 	#dets_path = sys.argv[2]
 	#show_boxes()
-<<<<<<< HEAD
 	'''	
 	video_dir = sys.argv[1]
 	det_path = sys.argv[2]
 	show_video(video_dir, det_path)
-=======
-	
-	#video_dir = sys.argv[1]
-	#det_path = sys.argv[2]
-	#show_boxes3('./img1', './refine_res1.txt')
->>>>>>> 826c5be7aab7f79158cc1c26a1bfd42b9cd1bf60
-	
 	'''
-	assert len(sys.argv) > 1, 'please input (video_dir, det_path, out_dir)'
+	'''	
+	#assert len(sys.argv) > 1, 'please input (video_dir, det_path, out_dir)'
 	video_dir = sys.argv[1]
 	det_path = sys.argv[2]
 	out_dir = sys.argv[3]
-<<<<<<< HEAD
 	show_tracks(video_dir, det_path, out_dir)
-=======
-    '''
-	show_tracks('./img', det_path, out_dir)
-	
->>>>>>> 826c5be7aab7f79158cc1c26a1bfd42b9cd1bf60
-	
+	'''
 
+	#post_processing(sys.argv[1], sys.argv[2], sys.argv[3])
+	refine(sys.argv[1], sys.argv[2])
 
 
 
